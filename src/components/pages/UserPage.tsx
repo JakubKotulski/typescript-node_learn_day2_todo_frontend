574d2ad6afa04238dadf4ab59158e97100e99df0
Jakub Kotulski <jakubkotulski@MacBook-Air-Jakub.local>
2025-09-04 19:29:35 +0200

    setup UserMainPage

---
+++ src/components/pages/UserPage.tsx
@@ -0,0 +1,12 @@
+import "./UserPage.css";
+
+const UserPage = () => {
+  return (
+    <div className="user-page">
+      <div className="tasks-info-section"></div>
+      <div className="add-task-section"></div>
+    </div>
+  );
+};
+
+export default UserPage;
